67e961063b6053f1faa312a1ea4ebda8d0e78794
TomPrint <tomekklewicki@gmail.com>
2025-08-07 00:10:59 +0200

    add sections aos and contact

--- hairdresser/src/app/pages/home/home.ts
+++ hairdresser/src/app/pages/home/home.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Slider } from "../../shared/slider/slider";
 import { Navbar } from "../../layout/navbar/navbar";
+import AOS from 'aos';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [Slider, Navbar],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit {
 
+ngOnInit(): void {
+  AOS.init({
+    duration: 700,
+    easing: 'ease-in-out',
+    once: false // allow animations every time the element is visible
+  });
+}
 }
